c999839e1869932df4fd2b186dd5954abf71e25f
Bruno Duarte <bruno.duarte314@gmail.com>
2025-03-27 00:04:30 -0300

    build: add @types/node

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@contexts": path.resolve(__dirname, "src/contexts"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@models": path.resolve(__dirname, "src/models"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@storage": path.resolve(__dirname, "src/storage"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@themes": path.resolve(__dirname, "src/themes"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
+});
